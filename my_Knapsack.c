e5dd8979f6b930e80cfe273fc8df6ca0beee79cf
OmarKabha49 <155442163+OmarKabha49@users.noreply.github.com>
2024-02-11 22:32:44 +0200

    Update my_Knapsack.c

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -33,89 +33,26 @@ int knapSack(int weights[], int values[], int selected_bool[]) {
     return table[ARR_SIZE][MAX_WEIGHT];
 }
 
-void print_example1(){
-    int weights[ARR_SIZE], values[ARR_SIZE], selected_bool[ARR_SIZE] = {0};
-
-    // example 1 Input
-    int example1_values[] = {35, 60, 100, 120, 130};
-    int example1_weights[] = {2, 5, 6, 9, 10};
-
-    for (int i = 0; i < ARR_SIZE; i++) {
-        weights[i] = example1_weights[i];
-        values[i] = example1_values[i];
-    }
-
-    int max_value = knapSack(weights, values, selected_bool);
-
-    printf("Maximum profit: %d\n", max_value);
-    printf("Items that give the maximum profit: [");
-    for (int i = ARR_SIZE - 1; i >= 0; i--) {
-        if (selected_bool[i]) {
-            printf("%c, ", 'A' + i);
-        }
-    }
-    printf("\b\b]\n"); //remove the trailing comma and space
-}
-
-void print_example2(){
-    int weights[ARR_SIZE], values[ARR_SIZE], selected_bool[ARR_SIZE] = {0};
-
-    
-     //example 2 Input
-     int example2_values[] = {35, 60, 100, 120, 130};
-     int example2_weights[] = {2, 5, 6, 9, 9};
-   
-
-    for (int i = 0; i < ARR_SIZE; i++) {
-        weights[i] = example2_weights[i];
-        values[i] = example2_values[i];
-    }
-
-    int max_value = knapSack(weights, values, selected_bool);
-
-    printf("Maximum profit: %d\n", max_value);
-    printf("Items that give the maximum profit: [");
-    for (int i = ARR_SIZE - 1; i >= 0; i--) {
-        if (selected_bool[i]) {
-            printf("%c, ", 'A' + i);
-        }
-    }
-    printf("\b\b]\n"); //remove the trailing comma and space
-}
-
-void print_example3(){
-    int weights[ARR_SIZE], values[ARR_SIZE], selected_bool[ARR_SIZE] = {0};
-
-
-    // example 3 Input
-    int example3_values[] = {35, 60, 100, 120, 130};
-    int example3_weights[] = {2, 5, 6, 9, 7};
-
-
-    for (int i = 0; i < ARR_SIZE; i++) {
-        weights[i] = example3_weights[i];
-        values[i] = example3_values[i];
-    }
-
-    int max_value = knapSack(weights, values, selected_bool);
-
-    printf("Maximum profit: %d\n", max_value);
-    printf("Items that give the maximum profit: [");
-    for (int i = ARR_SIZE - 1; i >= 0; i--) {
-        if (selected_bool[i]) {
-            printf("%c, ", 'A' + i);
-        }
-    }
-    printf("\b\b]\n"); //remove the trailing comma and space
-}
-
 int main() {
-    
-    print_example1();
-    printf("\n");
-    print_example2();
-    printf("\n");
-    print_example3();
+    int weights[ARR_SIZE], values[ARR_SIZE], selected_bool[ARR_SIZE] = {0};
+
+
+    for (int i = 0; i < ARR_SIZE; ++i) {
+        char name;
+        scanf(" %c %d %d", &name, &values[i], &weights[i]);
+    }
+
+
+    int max_value = knapSack(weights, values, selected_bool);
+
+    printf("Maximum profit: %d\n", max_value);
+    printf("Selected items: ");
+    for (int i = 0 ; i < ARR_SIZE;i++) {
+        if (selected_bool[i]) {
+            printf("%c ", 'a' + i);
+        }
+    }
+    printf("\b\b\n"); //remove the trailing comma and space
 
     return 0;
 }
